8ecde77b9599be9eb9f06fd8fbd478b1d4179f16
Lukitas <lucas.r.abimorad@gmail.com>
2023-10-05 16:01:06 -0300

    pequeños arreglos

--- 2_principalMod/app.js
+++ 2_principalMod/app.js
@@ -1,19 +1,18 @@
-const showBtns = document.querySelectorAll(".dropdown_btn");
-const dropdownBtn = document.querySelector("#container__fullname");
-const subMenu = document.querySelector("#container__subMenu");
+const showBtns = document.querySelectorAll('.dropdown_btn')
+const dropdownBtn = document.querySelector('#container__fullname')
+const subMenu = document.querySelector('#container__subMenu')
 
-showBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const incident_information =
-      e.currentTarget.parentElement.parentElement.nextElementSibling;
-    const icon = e.currentTarget.children[0];
-    incident_information.classList.toggle("incident__information-hidden");
-    icon.classList.toggle("active");
-  });
+showBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const incident_information = e.currentTarget.parentElement.parentElement.nextElementSibling;
+        const icon = e.currentTarget.children[0];
+        incident_information.classList.toggle('incident__information-hidden')
+        icon.classList.toggle('active')
+    })
 });
 
-dropdownBtn.addEventListener("click", (e) => {
-  const icon = e.currentTarget.children[0];
-  subMenu.classList.toggle("subMenu-hidden");
-  icon.classList.toggle("active");
-});
+dropdownBtn.addEventListener('click', e => {
+    const icon = e.currentTarget.children[0]
+    subMenu.classList.toggle('subMenu-hidden')
+    icon.classList.toggle('active')
+})
