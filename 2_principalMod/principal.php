58601325717ac72399fb49b923a6398185ae4c54
zequui <ezequielriv20@gmail.com>
2023-10-06 14:02:21 -0300

    asdad

--- 2_principalMod/principal.php
+++ 2_principalMod/principal.php
@@ -25,11 +25,11 @@
             echo '<p id="container__fullname">'.$usuario->getNombre().' '.$usuario->getApellido().'<i class="fa-solid fa-chevron-down"></i></p>';
         ?>
     </div>
-    <div id="emergent">
+    <div class="emergent">
         <div id="emergent__subMenu" class="subMenu-hidden">
             <a href="../controladores/exitSession.php" class="subMenu__option"><i class="fa-solid fa-right-from-bracket fa-lg"></i>Cerrar sesion</a>
         </div>
-        <h1 id=emergent__title>Incidentes emergentes</h1>
+        <h1 class=emergent__title>Incidentes emergentes</h1>
         <?php
         include_once '../controladores/getIncidents.php';
         include_once '../controladores/loadFiles.php';
@@ -48,8 +48,8 @@
             echo '
             <div class="emergent__incident" id="'.$incident->getID().'">
                 <div class="incident__title">
-                <p id="incident__name">'.$incident->getTitulo().'</p>
-                    <div id="incident__container">
+                <p class="incident__name">'.$incident->getTitulo().'</p>
+                    <div class="title__container">
                         <button class="container__button"><i class="fa-solid fa-x fa-xl"></i></i></button>
                         <button class="container__button"><i class="fa-solid fa-check fa-2xl"></i></button>
                         <button class="container__button dropdown_btn"><i class="fa-solid fa-arrow-down-long fa-2xl"></i></button>
@@ -58,17 +58,18 @@
                 <div class="inicident__information incident__information-hidden">
                     <div class="information__col">
                         <label>Descripcion</label>
-                        <p class="description__p">'.$incident->getDescripcion().'</p>
+                        <p class="col__p">'.$incident->getDescripcion().'</p>
                     </div>
                     <div class="information__col">
                         <label>Fecha</label>
-                        <div class="description__p">'.$incident->getFecha().'</div>
+                        <p class="col__p">'.$incident->getFecha().'</p>
                         <div></div>
                         <label>Tipo</label>
-                        <div class="description__p">'.$incident->getTipo().'</div>';
+                        <p class="col__p">'.$incident->getTipo().'</p>';
+                        
                         if(!empty($incident->getArchivo())){
                          echo ' <label>Archivo a descargar</label>
-                         <div class="description__p"><a href="'.loadFile($incident).'" download="archvoRelevante'.$incident->getID().'">Descargar</a></div>';
+                         <p class="col__p"><a href="'.loadFile($incident).'" download="archvoRelevante'.$incident->getID().'">Descargar</a></p>';
                         }
                        
                     echo '</div>
@@ -87,6 +88,48 @@
         ';
         }
         ?>
+    </div>
+    <br>
+    <div class="emergent">
+        <h1 class=emergent__title>Incidentes en curso</h1>
+        <div class="emergent__incident" id="'.$incident->getID().'">
+                <div class="incident__title">
+                    <p class="incident__name">'.$incident->getTitulo().'</p>
+                        <div id="incident__container">
+                            <button class="container__button"><i class="fa-solid fa-x fa-xl"></i></i></button>
+                            <button class="container__button"><i class="fa-solid fa-check fa-2xl"></i></button>
+                            <button class="container__button dropdown_btn"><i class="fa-solid fa-arrow-down-long fa-2xl"></i></button>
+                        </div>
+                </div>
+                <div class="inicident__information incident__information-hidden">
+                    <div class="information__col">
+                        <label>Descripcion</label>
+                        <p class="col__p">'.$incident->getDescripcion().'</p>
+                    </div>
+                    <div class="information__col">
+                        <label>Fecha</label>
+                        <p class="col__p">'.$incident->getFecha().'</p>
+                        <div></div>
+                        <label>Tipo</label>
+                        <p class="col__p">'.$incident->getTipo().'</p>';
+                        
+                        <label>Archivo a descargar</label>
+                         <p class="col__p"><a href="'.loadFile($incident).'" download="archvoRelevante'.$incident->getID().'">Descargar</a></p>';
+                    </div>
+                    <div class="information__col">
+                        <label>Nombre y Apellido</label>
+                        <p class="col__p">'.$denunciante->getNombre().' '.$denunciante->getApellido().'</p>
+
+                        <label>Cedula</label>
+                        <p class="col__p">'.$denunciante->getCi().'</p>
+                        
+                        <label>Telefono</label>
+                        <p class="col__p">'.$denunciante->getTelefono().'</p>
+                    </div>
+                    <div class="incident__title">
+                    </div>
+                </div>
+            </div>
     </div>
     <script src="app.js"></script>
 </body>
